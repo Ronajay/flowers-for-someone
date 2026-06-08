5d3fcb5ed520fddf0b2e7709911a67c3ad41c6e1
Rona <florrona3@gmail.com>
2026-06-09 00:21:17 +0800

    Change title text from 'FLOWER FOR YOU' to empty

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('FLOWER FOR YOU').split('')
+    const titles = (' ').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
