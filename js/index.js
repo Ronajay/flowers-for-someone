5a445e8286cb703125ab2e360625a498df0dc424
Rona <florrona3@gmail.com>
2026-06-09 00:20:59 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = `I Have Something for you arianna`.split('')
+const text = `.`.split('')
 
 // Create container for better responsive layout
 title.style.display = 'flex'
